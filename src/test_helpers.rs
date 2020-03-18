d077552f3b4964a119f74ea5b5a9c6b4f42a5f5d
Pascal Hertleif <killercup@gmail.com>
2020-03-18 18:45:39 +0100

    Some packaging tweaks
    Can't trust the tar

--- src/test_helpers.rs
+++ src/test_helpers.rs
@@ -10,11 +10,13 @@ pub use std::{
 pub use tempfile::tempdir;
 
 pub fn random_file(path: impl AsRef<Path>) -> Result<Vec<u8>> {
+    let path = path.as_ref();
     let mut rng = rand::thread_rng();
     let mut raw_content = vec![0u8; 1024];
     rng.try_fill(&mut raw_content[..])?;
     let content = zstd::stream::encode_all(Cursor::new(&raw_content[..]), 3)?;
 
+    fs::create_dir_all(path.parent().context("parent dir")?).context("mkdir")?;
     fs::write(path, content).context("write file")?;
     Ok(raw_content)
 }
