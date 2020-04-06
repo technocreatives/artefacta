ced26ddb7e2d42470a11da20cf2a851ed257bf6f
Pascal Hertleif <pascal@technocreatives.com>
2020-04-06 14:46:06 +0200

    Add test: upgrade to a build already cached

--- tests/cli.rs
+++ tests/cli.rs
@@ -28,3 +28,31 @@ fn install_build_from_remote_directory() {
         "symlink points to new build"
     );
 }
+
+#[test]
+fn upgrade_to_a_build_already_cached() {
+    let (local, remote) = init();
+    let (local, remote) = (local.path(), remote.path());
+
+    fs::write(remote.join("build1.tar.zst"), b"foobar").unwrap();
+    fs::write(remote.join("build2.tar.zst"), b"foobarbaz").unwrap();
+
+    // "cache" the builds locally
+    fs::copy(remote.join("build1.tar.zst"), local.join("build1.tar.zst")).unwrap();
+    fs::copy(remote.join("build2.tar.zst"), local.join("build2.tar.zst")).unwrap();
+
+    // "install" build1
+    std::os::unix::fs::symlink(local.join("build1.tar.zst"), local.join("current")).unwrap();
+
+    artefacta(local, remote)
+        .args(&["install", "build2"])
+        .assert()
+        .success();
+
+    let current = local.join("current");
+    assert_eq!(
+        local.join("build2.tar.zst"),
+        fs::read_link(&current).unwrap(),
+        "symlink points to new build"
+    );
+}
