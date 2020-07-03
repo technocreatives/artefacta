e1c8bc3b92a0f3791e0e109ee9ec8f29ff3d4802
Pascal Hertleif <pascal@technocreatives.com>
2020-07-03 13:06:02 +0200

    version doodles

--- tests/git.rs
+++ tests/git.rs
@@ -5,6 +5,8 @@ use test_helpers::*;
 fn find_related_tags_in_repo() {
     let repo = tempdir().unwrap();
     run("git init .", &repo.path());
+    run("git config user.email 'git-test@example.com'", &repo.path());
+    run("git config user.name 'Author Name'", &repo.path());
 
     run("echo foo > wtf", &repo.path());
     run("git add .", &repo.path());
@@ -16,6 +18,11 @@ fn find_related_tags_in_repo() {
     run("git commit -m 'bump build2'", &repo.path());
     run("git tag build2", &repo.path());
 
+    run("echo baz > wtf", &repo.path());
+    run("git add .", &repo.path());
+    run("git commit -m 'bump build3'", &repo.path());
+    run("git tag build3", &repo.path());
+
     ls(&repo.path());
 
     run("git tag -l", &repo.path());
@@ -25,26 +32,6 @@ fn find_related_tags_in_repo() {
     let mut tags = get_tags(&r).unwrap();
     tags.sort_by(|a, b| a.time.cmp(&b.time));
     dbg!(tags);
-    // let tags = r.tag_names(None).unwrap();
-    // dbg!(tags.iter().flatten().collect::<Vec<_>>());
-
-    // for tag in tags.iter() {
-    //     let tag = tag.unwrap();
-    //     // let r = r.find_reference(tag).unwrap();
-    //     // if r.is_tag() { continue; }
-    //     // dbg!(r.name());
-    // }
-
-    // let refs = r
-    //     .references()
-    //     .unwrap()
-    //     .filter_map(|x| x.ok())
-    //     .filter(|r| r.is_tag());
-    // for reference in refs {
-    //     dbg!(reference.name(), reference.shorthand());
-    //     let tag = reference.peel_to_commit().unwrap();
-    //     dbg!(tag.summary());
-    // }
 }
 
 #[derive(Debug, Clone)]
@@ -73,4 +60,45 @@ fn get_tags(repo: &git2::Repository) -> Result<Vec<Tag>> {
             })
         })
         .collect()
+}
+
+#[test]
+fn tags_to_patch_from() {
+    let tags = vec![
+        "IL40.0.0".to_string(),
+        "IL40.0.1".to_string(),
+        "IL40.1.0".to_string(),
+        "IL40.2.17".to_string(),
+        "IL40.2.18".to_string(),
+    ];
+    let current_tag = "IL40.2.19";
+    let patch_these = find_tags_to_patch(current_tag, &tags).unwrap();
+    assert_eq!(
+        patch_these,
+        vec!["IL40.2.18".to_string(), "IL40.1.0".to_string()]
+    );
+}
+
+fn find_tags_to_patch(current: &str, tags: &[String]) -> Result<Vec<String>> {
+    use smol_str::SmolStr;
+    use std::collections::BTreeMap;
+
+    fn tag_to_slice(tag: &str) -> Vec<SmolStr> {
+        tag.split('.').map(SmolStr::from).collect()
+    }
+
+    let current = tag_to_slice(current);
+    let parsed_tags: Vec<Vec<SmolStr>> = tags.iter().map(|tag| tag_to_slice(tag)).collect();
+
+    let mut to_patch = vec![];
+
+    if let Some(last_num) = current.iter().last() {
+        todo!("sub to max 0");
+    }
+
+    if let Some(second_to_last_num) = current.iter().rev().nth(1) {
+        todo!("sub to max 0");
+    }
+
+    Ok(to_patch)
 }
